137414dedc9b164cdf37d90de84a74aa7810181a
Deep Datta Roy <deepdattaroy8888@gmail.com>
2014-03-21 19:11:46 -0400

    Added test script

---
+++ test.sh
@@ -0,0 +1,5 @@
+java -jar client.jar localhost 51000 jobs.jar jobs.Hello &
+java -jar client.jar localhost 51000 jobs.jar jobs.Hello &
+java -jar client.jar localhost 51000 jobs.jar jobs.Hello &
+java -jar client.jar localhost 51000 jobs.jar jobs.Hello
+
